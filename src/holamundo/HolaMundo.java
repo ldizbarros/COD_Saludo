64972d056292e49deb43610b43638c97e3ccd9c0
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-10-05 09:48:55 +0200

    Inicio Proyecto
    Add 2 lines

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
     
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        System.out.println("Hello World!");
+        System.out.println("Hallo Welt!");
     }
     
 }
